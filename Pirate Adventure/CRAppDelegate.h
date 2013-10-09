31fee74a7bd81a98a8e876db64c0f3113992840f
Chris Roche <chrisroche@Chris-Roche-MacBook-Pro.local>
2013-10-09 16:32:12 -0500

    Initial Commit

---
+++ Pirate Adventure/CRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CRAppDelegate.h
+//  Pirate Adventure
+//
+//  Created by Chris Roche on 10/9/13.
+//  Copyright (c) 2013 Chris Roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Adventure/CRViewController.h
@@ -0,0 +1,13 @@
+//
+//  CRViewController.h
+//  Pirate Adventure
+//
+//  Created by Chris Roche on 10/9/13.
+//  Copyright (c) 2013 Chris Roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRViewController : UIViewController
+
+@end
